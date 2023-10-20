ea5cba6e4529a3d6a168fceca5a3862b596103ad
tranvy57 <tranvy.art@gmail.com>
2023-10-20 14:34:24 +0700

    start

--- src/mainFrame/mainFrame.java
+++ src/mainFrame/mainFrame.java
@@ -37,7 +37,6 @@ public class mainFrame extends javax.swing.JFrame{
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(1920, 1030));
         setMinimumSize(new java.awt.Dimension(1920, 1030));
-        setPreferredSize(new java.awt.Dimension(1920, 1030));
         setResizable(false);
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
