8bb389e62d5a5d0fa993a08243d8574644ece1fa
Hieu Lai <ldmhieudev@gmail.com>
2023-11-19 13:31:53 +0700

    thead auto hủy phòng sau 2 giờ không nhận phòng

--- src/dao/DAO_HoaDon.java
+++ src/dao/DAO_HoaDon.java
@@ -155,6 +155,38 @@ public class DAO_HoaDon implements I_CRUD<HoaDon>{
         }
         return result;
     }
+    
+    public boolean updateTrangThaiHoaDonBangMaHoaDon(String maHoaDon, int TrangThai) {
+        ConnectDB.getInstance();
+        Connection con = ConnectDB.getConnection();
+        PreparedStatement statement = null;
+        int n = 0;
+          try {
+
+              statement = con.prepareStatement("update HoaDon\n" +
+                                                "set trangThai = ? \n" +
+                                                "where maHoaDon = ? ");
+
+              statement.setInt(1, TrangThai);
+
+              statement.setString(2, maHoaDon);
+
+              n = statement.executeUpdate();
+
+          } catch (Exception e) {
+              // TODO: handle exception
+              e.printStackTrace();
+
+          } finally {
+              try {
+                  statement.close();
+              } catch (Exception e2) {
+                  // TODO: handle exception
+                  e2.printStackTrace();
+              }
+          }
+          return n > 0;
+      }
 
     public ThongTinPhongDangChon getThongTinTraPhong(String maPhong){
         ThongTinPhongDangChon result = new ThongTinPhongDangChon();
